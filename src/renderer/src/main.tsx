82a18ada3e8e544218ee26f51b9ce66548f57703
inkyu.choi <chldlsrb1000@naver.com>
2025-12-27 20:43:31 +0900

    리액트 쿼리 추가

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,11 +1,26 @@
 import './assets/main.css'
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './components/App'
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+      gcTime: 0,
+      refetchOnMount: true,
+      refetchOnWindowFocus: false,
+      retry: false
+    }
+  }
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 )
